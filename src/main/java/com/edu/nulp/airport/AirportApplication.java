e1ea0deef322b6727db1bf9647cddcbcacf4c039
kyrylo <kyrylo0629@gmail.com>
2021-12-02 19:47:14 +0200

    test

--- src/main/java/com/edu/nulp/airport/AirportApplication.java
+++ src/main/java/com/edu/nulp/airport/AirportApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AirportApplication {
-
+    //test
     public static void main(String[] args) {
         SpringApplication.run(AirportApplication.class, args);
     }
