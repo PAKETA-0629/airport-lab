0619785212613c233b5845ddae518721c2e0e1f3
kyrylo <kyrylo0629@gmail.com>
2021-12-02 20:13:01 +0200

    add User model

--- src/main/java/com/edu/nulp/airport/model/User.java
+++ src/main/java/com/edu/nulp/airport/model/User.java
@@ -1,4 +1,25 @@
 package com.edu.nulp.airport.model;
 
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    private Long id;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "role_id")
+    private Long roleId;
+    @Column(name = "is_active")
+    private Boolean isActive;
 }
