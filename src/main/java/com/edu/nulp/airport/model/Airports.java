e6954b639df15f74ada3ebd9dc767e5194fd3beb
kyrylo <kyrylo0629@gmail.com>
2021-12-02 23:25:27 +0200

    add airport model

--- src/main/java/com/edu/nulp/airport/model/Airports.java
+++ src/main/java/com/edu/nulp/airport/model/Airports.java
@@ -1,2 +1,34 @@
-package com.edu.nulp.airport.model;public class Airports {
+package com.edu.nulp.airport.model;
+
+import lombok.Data;
+import com.edu.nulp.airport.model.City;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "airports")
+public class Airports {
+
+    @Id
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "iata")
+    private String iata;
+    @Column(name = "ukll")
+    private String ukll;
+
+    @ManyToOne
+    @JoinColumn(name = "city", referencedColumnName = "id")
+    @ToString.Include
+    private City city;
+
+    @Column(name = "latitude")
+    private Double latitude;
+    @Column(name = "longitude")
+    private Double longitude;
+
+
 }
