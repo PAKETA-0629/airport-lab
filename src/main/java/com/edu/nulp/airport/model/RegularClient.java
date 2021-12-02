6f3d49b81d1e8835be49489fae57d203d586bfef
kyrylo <kyrylo0629@gmail.com>
2021-12-02 23:58:06 +0200

    add regular client model

--- src/main/java/com/edu/nulp/airport/model/RegularClient.java
+++ src/main/java/com/edu/nulp/airport/model/RegularClient.java
@@ -1,2 +1,26 @@
-package com.edu.nulp.airport.model;public class RegularClient {
+package com.edu.nulp.airport.model;
+
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "regular_clients")
+public class RegularClient {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @ToString.Exclude
+    private User user;
+    @Column
+    private Long points;
+    @Column(name = "flight_distance")
+    private Long flightDistance;
 }
