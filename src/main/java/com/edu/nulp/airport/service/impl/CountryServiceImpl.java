c6760e63eb15e6d47a5d574ed87c522f557d600e
kyrylo <kyrylo0629@gmail.com>
2021-12-03 13:17:20 +0200

    impl country service

--- src/main/java/com/edu/nulp/airport/service/impl/CountryServiceImpl.java
+++ src/main/java/com/edu/nulp/airport/service/impl/CountryServiceImpl.java
@@ -1,8 +1,31 @@
 package com.edu.nulp.airport.service.impl;
 
+import com.edu.nulp.airport.model.Country;
+import com.edu.nulp.airport.repository.CountryRepository;
 import com.edu.nulp.airport.service.CountryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@Slf4j
 public class CountryServiceImpl implements CountryService {
+
+    private final CountryRepository countryRepository;
+
+    public CountryServiceImpl(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @Override
+    public Country findByName(String name) {
+        Optional<Country> optionalCountry = countryRepository.findByName(name);
+        if (!optionalCountry.isPresent()) {
+            //todo add exception and handler exception
+            log.debug("Country Not Exits");
+        }
+        return optionalCountry.get();
+    }
 }
