5a723c7c95035e7a839c1fd797104169e2847c56
kyrylo <kyrylo0629@gmail.com>
2021-12-03 13:27:41 +0200

    add country controller

---
+++ src/main/java/com/edu/nulp/airport/controller/CountryController.java
@@ -0,0 +1,24 @@
+package com.edu.nulp.airport.controller;
+
+import com.edu.nulp.airport.model.Country;
+import com.edu.nulp.airport.service.impl.CountryServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    private final CountryServiceImpl countryService;
+
+    @Autowired
+    public CountryController(CountryServiceImpl countryService) {
+        this.countryService = countryService;
+    }
+
+    @GetMapping("/country/{name}")
+    public Country getCountryByName(@PathVariable("name") String name) {
+        return countryService.findByName(name);
+    }
+}
