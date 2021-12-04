08d2516fcd6fea72a3eb714b450ee0447932cdcd
kyrylo <kyrylo0629@gmail.com>
2021-12-05 00:28:46 +0200

    add exception handler

---
+++ src/main/java/com/edu/nulp/airport/exception/NotExistException.java
@@ -0,0 +1,17 @@
+package com.edu.nulp.airport.exception;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class NotExistException extends IllegalArgumentException {
+    private static final long serialVersionUID = 1L;
+    private static final String NOT_EXIST_EXCEPTION = "Not exist";
+
+    public NotExistException(String message) {
+        super(message.isEmpty() ? NOT_EXIST_EXCEPTION : message);
+    }
+
+    public NotExistException() {
+        super(NOT_EXIST_EXCEPTION);
+    }
+}
+
